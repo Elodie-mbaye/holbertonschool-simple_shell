72eafb9a7870b1ac876c255a8023e6e1c6dbe4f5
root <root@ElodieSeck.localdomain>
2024-12-20 14:51:29 +0100

    File that contents all the prototypes and functions

---
+++ shell.h
@@ -0,0 +1,12 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+void  shell_interactive(void);
+void  shell_no_interactive(void);
+char *read_line(void)
+#endif
