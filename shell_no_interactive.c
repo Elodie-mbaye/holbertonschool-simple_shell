b3d9d77a016ab9374c4c9cba09124f539616f35c
root <root@ElodieSeck.localdomain>
2024-12-20 16:07:37 +0100

    function shell no_interactive

---
+++ shell_no_interactive.c
@@ -0,0 +1,27 @@
+#include "shell.h"
+/**
+ * shell_no_interactive - line interpreter
+ *
+ * Return: void
+ *
+ */
+void shell_no_interactive(void)
+{
+	char *line;
+	char **args;
+	int status = -1;
+
+	do {
+		line = read_stream();
+		args = parse_line(line);
+		status = execute_args(args);
+
+		free(line);
+		free(args);
+
+		if (status >= 0)
+		{
+			exit(status);
+		} while (status == -1);
+	}
+}
