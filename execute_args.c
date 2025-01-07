09a6ddf9b7e32e2bb8ccaa20f17a595aed441708
root <root@ElodieSeck.localdomain>
2025-01-07 15:25:11 +0100

    modification of fonction execute

--- execute_args.c
+++ execute_args.c
@@ -1,51 +1,44 @@
 #include "shell.h"
-#include <string.h>
-#include <sys/wait.h>
+#include <stdio.h>
+#include <unistd.h>
 
 /**
- * execute_args - execute a command as a process
- * @args: command and its flags
+ * execute - Execute a command and its arguments in a separate process.
+ * @argv: Array of strings containing the command and arguments to execute.
  *
- * Return: 1 if succefull, 0 otherwise
+ * Return: 0 on success, -1 or 127 on failure.
  */
 int execute_args(char **args)
 {
-	pid_t pid;
 	int status;
+	char *env[] = {NULL};
+	pid_t pid;
 
-	char *path = NULL;
+	if (args == NULL)
+		return (-1);
 
-	if (!args[0])
-		return (1);
 
-	if (strcmp(args[0], "exit") == 0)
-		return (0);
-	else if (strcmp(args[0], "env") == 0)
-		print_env();
+	pid = fork();
+
+	if (pid == -1)
+	{
+		free(args);
+		perror("Error");
+		_exit(-1);
+	}
+
+	if (pid == 0)
+	{
+		if (execve(args[0], args, env) == -1)
+		{
+			free(args);
+			perror("Execution failed");
+			_exit(127);
+		}
+	}
 
 	else
-	{
-		path = is_a_command(args[0]);
+		wait(&status);
 
-		pid = fork();
-
-		if (pid == 0)
-		{
-			if (execve(args[0], args, NULL) == -1)
-				perror("error during child process");
-			exit(EXIT_FAILURE);
-		}
-		else if (pid < 0)
-			perror("error");
-		else
-		{
-			do {
-				waitpid(pid, &status, WUNTRACED);
-			} while (!WIFEXITED(status) && !WIFSIGNALED(status));
-		}
-
-		if (path)
-			free(path);
-	}
-	return (1);
+	return (0);
 }
